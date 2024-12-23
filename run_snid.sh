7d5c4cff83d38d454ae3f8e311b9b58e52285373
Robert Fisher <robert.fisher@umassd.edu>
2024-12-23 08:26:32 -0500

    Updated file to incorporate XMing.

--- run_snid.sh
+++ run_snid.sh
@@ -60,9 +60,10 @@ if [[ "$PLATFORM" == "macOS" ]]; then
     xhost + 127.0.0.1
 
 elif [[ "$PLATFORM" == "Windows" ]]; then
-    # Define default installation paths for VcXsrv and Xming
+    # Define default installation paths for VcXsrv, Xming, and Cygwin/X. Update if installed elsewhere.
     VCS_PATH="/mnt/c/Program Files/VcXsrv/vcxsrv.exe"
     XMING_PATH="/mnt/c/Program Files (x86)/Xming/Xming.exe"
+    CYGWIN_X_PATH="/mnt/c/cygwin64/bin/XWin.exe" 
 
     # Initialize XSERVER_PATH variable
     XSERVER_PATH=""
@@ -71,11 +72,16 @@ elif [[ "$PLATFORM" == "Windows" ]]; then
     if [ -f "$VCS_PATH" ]; then
         XSERVER_PATH="$VCS_PATH"
         echo "Detected VcXsrv at $VCS_PATH"
+    # Or XMing
     elif [ -f "$XMING_PATH" ]; then
         XSERVER_PATH="$XMING_PATH"
         echo "Detected Xming at $XMING_PATH"
+    # Check if Cygwin/X is installed
+    elif [ -f "$CYGWIN_X_PATH" ]; then
+        XSERVER_PATH="$CYGWIN_X_PATH"
+        echo "Detected Cygwin/X at $CYGWIN_X_PATH"
     else
-        echo "Neither VcXsrv nor Xming is installed."
+        echo "None of VcXsrv, Xming, or Cygwin/X is installed."
         echo "Please install one of them to proceed."
         exit 1
     fi
@@ -101,6 +107,10 @@ elif [[ "$PLATFORM" == "Windows" ]]; then
     elif [[ "$XSERVER_PATH" == "$XMING_PATH" ]]; then
         # Launch Xming with desired options
         "/mnt/c/Program Files (x86)/Xming/Xming.exe" :0 -multiwindow -clipboard &
+        XSERVER_PID=$!
+    elif [[ "$XSERVER_PATH" == "$CYGWIN_X_PATH" ]]; then
+        # Launch Cygwin/X with desired options
+        "/mnt/c/cygwin64/bin/XWin.exe" :0 -multiwindow -clipboard &
         XSERVER_PID=$!
     fi
 
@@ -136,11 +146,17 @@ else
     exit 1
 fi
 
+echo "Building Docker container..."
+docker build -t snid-app --progress=plain -f snid_dockerfile .
+
 # Run the Docker container with X11 forwarding
 echo "Running Docker container 'snid-app'..."
+
 docker run -it --rm \
-    -e DISPLAY=$DISPLAY \
+    --name snid-container \
+    -e DISPLAY=host.docker.internal:0 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v ~/Desktop/snid:/home/sniduser/snid-5.0/desktop \
     snid-app
 
 # Cleanup will be called automatically due to the trap
